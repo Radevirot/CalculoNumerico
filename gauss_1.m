721eedcccdc43ecc784e76c3b3ffb474a33ed17a
Radevirot <78053632+Radevirot@users.noreply.github.com>
2023-04-11 16:23:30 -0300

    Añadida la función de Gauss
    Se creó el archivo con la función de Gauss, queda pendiente describir el código en detalle.

---
+++ gauss_1.m
@@ -0,0 +1,24 @@
+function [x] = gauss1(A,b)
+  n=length(b);
+  A=[A b];
+  % Eliminacion
+  for k=1:n-1
+
+    for i=k+1:n
+
+      m = A(i,k)/A(k,k);
+
+      A(i,k)=0;
+
+        for j=k+1:n+1
+
+          A(i,j) = A(i,j)-m*A(k,j);
+
+        endfor
+      endfor
+  endfor
+
+
+  if (A(n,n)==0)
+    disp(’no hay sol. unica’)
+  endif

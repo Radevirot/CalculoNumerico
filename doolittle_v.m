b757f55f2f11881066b7c302bba367b2a182019b
Radevirot <78053632+Radevirot@users.noreply.github.com>
2024-04-10 17:03:36 -0300

    Doolittle vectorizado

--- doolittle_v.m
+++ doolittle_v.m
@@ -12,7 +12,7 @@ function [A, L, U] = doolittle_v(A)
   for k=1:n-1
 
     A(k+1:n,k) = A(k+1:n,k)/A(k,k);
-    A(k+1:n,k+1:n) = A(k+1:n,k+1:n) - A(k+1:n,k) * A(k,k+1:n);
+    A(k+1:n,k+1:n) -= A(k+1:n,k) * A(k,k+1:n);
 
 
   endfor

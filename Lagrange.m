9e8c22f1cdc60106b2ebc1c5c3ac6dabca6a8328
Radevirot <78053632+Radevirot@users.noreply.github.com>
2024-05-22 12:17:52 -0300

    .

--- Lagrange.m
+++ Lagrange.m
@@ -5,8 +5,16 @@ function [P, L] = Lagrange(x,y)
   L(:,n) = ones(n,1);
 
   for i = 1:n
-    for j = 1:n-1
-      %completar
+    for j=1:i-1
+      L(i,:) = (conv(L(i,:),[1,-x(j)]))(2:n+1)/(x(i) - x(j));
     endfor
+
+    for j = i+1:n
+      L(i,:) = (conv(L(i,:),[1,-x(j)]))(2:n+1)/(x(i) - x(j));
+    endfor
+    P = P + y(i)*L(i,:); # armamos el polinomio de lagrange
   endfor
+
+endfunction
+
 

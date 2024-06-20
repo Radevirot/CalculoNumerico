f8b4fd4651f1d10d624989afba3e7e19070bd9f6
Radevirot <78053632+Radevirot@users.noreply.github.com>
2024-06-20 16:22:42 -0300

    hb

---
+++ tp8ej2.m
@@ -0,0 +1,20 @@
+clear all; clc;
+c2 = (1/70)*(8-12*sin(log(2))-4*cos(log(2)))
+c1 = 11/10 - c2
+y_exacta = @(x) c1 .* x + (c2 ./ x.^2) - (3./10) .* sin(log(x)) - (1./10) .* cos(log(x))
+
+f = @(x) [-2./x , 2./(x.^2) , sin(log(x))./(x.^2)]
+
+inter = [1 2]
+yc = inter
+h = 0.1
+L = (2-1)/h
+
+[x y] = disparo_lineal(f, inter, yc, L);
+error_abs_h01 = norm(y-y_exacta(x),inf)
+
+h = 0.01
+L = (2-1)/h
+
+[x y] = disparo_lineal(f, inter, yc, L);
+error_abs_h001 = norm(y-y_exacta(x),inf)
